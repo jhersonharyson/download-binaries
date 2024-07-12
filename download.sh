3ff24cbc4f0cd91c61f1d90527ada2ae8a8f63bb
Jherson Haryson <jherson_k-f@hotmail.com>
2024-07-12 20:00:31 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,10 +2,12 @@
 
 # Verifica se o nome do projeto foi passado como parâmetro
 if [ -z "$1" ]; then
-  echo "Use: $0 <host>"
+  echo "Use: $0 <projectName>"
   exit 1
 fi
 
+# Parâmetro do script
+projectName="mgrowth-point-cms"
 
 # Obtém o hash do commit atual do Git
 commitHash="8d95014ba0e8257d55dbb9c53384e9420b11d562"
@@ -17,10 +19,10 @@ fi
 # URL do download
 #downloadUrl="https://local.adminml.com:8443/api/metrics/quality/download?projectName=${projectName}&commit=${commitHash}"
 #downloadUrl="https://api.mercadopago.com/mgrowth-quality/api/metrics/quality/download?projectName=${projectName}&commit=${commitHash}"
-downloadUrl="$0" #"https://quality-beta.adminml.com/api/metrics/quality/download?projectName=${projectName}&commit=${commitHash}"
+downloadUrl="https://quality-beta.adminml.com/api/metrics/quality/download?projectName=${projectName}&commit=${commitHash}"
 
 # Nome do arquivo para salvar o download
-downloadFile="binariesFiles.zip"
+downloadFile="${projectName}-${commitHash}.zip"
 
 # Diretório onde descompactar os arquivos
 outputDir="binaries"
