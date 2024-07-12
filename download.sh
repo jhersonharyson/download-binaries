cb5450d92825318892157de9f8be9f26b42822df
Jherson Haryson <jherson_k-f@hotmail.com>
2024-07-12 19:26:21 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Verifica se o nome do projeto foi passado como parâmetro
-if [ -z "$1" ]; then
-  echo "Use: $0 <host>"
+# Verifica se o URL de download e o nome do projeto foram passados como parâmetros
+if [ $# -ne 2 ]; then
+  echo "Use: $0 <url> <project-name>"
   exit 1
 fi
 
-# Parâmetro do script
-downloadUrl=$0
+# Parâmetros do script
+downloadUrl=$1
+projectName=$2
 
 # Obtém o hash do commit atual do Git
 commitHash=$(git rev-parse HEAD)
@@ -15,7 +16,6 @@ if [ -z "$commitHash" ]; then
   echo "Error: Cannot get current commit hash."
   exit 1
 fi
-
 
 # Nome do arquivo para salvar o download
 downloadFile="binaries.zip"
@@ -26,13 +26,13 @@ outputDir="binaries"
 # Faz o download do arquivo usando curl
 curl --location "$downloadUrl" --output $downloadFile
 
-# # Verifica o status do download
-# if [ "$response" -eq 200 ]; then
-#   echo "Successful Download"
-# else
-#   echo "Download Error. Status HTTP: $response"
-#   exit 1
-# fi
+# Verifica o status do download
+if [ $? -eq 0 ]; then
+  echo "Successful Download"
+else
+  echo "Download Error."
+  exit 1
+fi
 
 # Cria o diretório de saída se não existir
 mkdir -p "$outputDir"
@@ -42,9 +42,9 @@ unzip -o "$downloadFile" -d "$outputDir"
 
 # Verifica se a descompactação foi bem-sucedida
 if [ $? -eq 0 ]; then
-  echo "File unziped with success in directory: $outputDir."
+  echo "File unzipped with success in directory: $outputDir."
 else
-  echo "Error when try to unzip file."
+  echo "Error when trying to unzip file."
   exit 1
 fi
 
